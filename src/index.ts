d4d82dc62730c835ef3757d487d6927efbc31b2b
Djaymare <djayp34@gmail.com>
2024-04-25 11:51:27 +0200

    split plugin from exports

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,12 @@
-import { OhVueIcon, addIcons } from "oh-vue-icons"
-import { Plugin as VuePlugin } from "vue";
-import Editor from "./components/Editor.vue";
-import createRouter from "./router";
-import {
-    RiArrowGoBackLine,
-    RiArrowGoForwardLine,
-    RiFormatClear,
-    RiBold,
-    RiItalic,
-    RiLink,
-    BiBlockquoteLeft,
-    RiImageLine,
-    RiSeparator,
-    RiTextWrap,
-    RiAddLine,
-    RiDragMove2Fill
-} from "oh-vue-icons/icons";
-import EnkiView from "./components/EnkiView.vue";
-export { createRouter };
-export default {
-    install:(app, sitemap) => {
-        addIcons(
-            RiArrowGoBackLine,
-            RiArrowGoForwardLine,
-            RiFormatClear,
-            RiBold,
-            RiItalic,
-            RiLink,
-            BiBlockquoteLeft,
-            RiImageLine,
-            RiSeparator,
-            RiTextWrap,
-            RiAddLine,
-            RiDragMove2Fill
-        );
-        app
-            .component("Editor", Editor)
-            .component("EnkiView", EnkiView)
-            .component("v-icon", OhVueIcon)
-            .provide("sitemap", sitemap)
-    }
-} as VuePlugin
+import plugin from './plugin'
+import Editor from './components/Editor.vue';
+import EnkiView from './components/EnkiView.vue';
+import ListBase from './components/ListBase.vue';
+export { default as createRouter } from "./router";
+export type * from './types';
+export {
+    Editor,
+    EnkiView,
+    ListBase
+}
+export default plugin;
